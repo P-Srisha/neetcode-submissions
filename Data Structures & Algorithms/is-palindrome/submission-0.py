a8bff6fb4334ff4db2fb806e953363765e840ba3
P Naga Srisha <p.naga.srisha@gmail.com>
2026-07-31 18:42:51 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        st = ""
+        for c in s:
+            if c.isalnum():
+                st += c.lower()
+
+        print(st)
+
+        i, j = 0, len(st) - 1
+        while (i <= j):
+            if st[i] != st[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
